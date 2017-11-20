bd661008048975af4ad0118ed38e0f33c301f25c
snyambura <snyambura94@gmail.com>
2017-11-20 09:20:35 +0300

    autofocus

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,79 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+      <h3>Add new Task</h3>
+      <input v-focus type="text" v-model="message">
+      <input type="button" v-on:click="Add()" value="Add"/>
+
+      <h3>Pending Tasks </h3>
+      <p v-for="(task, index) in tasks">
+          <span v-text="task.name"></span>
+          <a href="#" v-on:click.prevent="deleteObject(index)">&times;</a>
+      </p>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    export default {
+        name: 'app',
+
+        directives: 'focus',
+
+        data() {
+            return {
+                tasks :[
+                    {
+                        "name": "attend training"
+                    },
+                    {
+                        "name": "write today's daily blog"
+                    },
+                    {
+                        "name": "do the vuejs report"
+                    },
+                    {
+                        "name": "write a simple vuejs application"
+                    },
+                    {
+                        "name": "complete the styling challenge"
+                    }
+
+                ]
+            };
+        },
+
+        methods: {
+            deleteObject: function(index) {
+                this.$delete(this.tasks, index);
+            },
+
+            Add() {
+                if(this.message != "") {
+                    //alert message
+                    this.tasks.unshift({
+                        name: this.message
+                        // status: false
+                    });
+                    this.message = '';
+                }
+
+            },
+
+
+        }
+
     }
-  }
-}
 </script>
 
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/*#app {*/
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+  /*-webkit-font-smoothing: antialiased;*/
+  /*-moz-osx-font-smoothing: grayscale;*/
+  /*text-align: center;*/
+  /*color: #2c3e50;*/
+  /*margin-top: 60px;*/
+/*}*/
 
 h1, h2 {
   font-weight: normal;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.directive('focus', {
+    // When the bound element is inserted into the DOM...
+    inserted: function (el) {
+        // Focus the element
+        el.focus()
+    }
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
